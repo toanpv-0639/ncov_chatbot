59e06081b69e3183cc7762a4e5ac8e646eea7265
toanpv-0639 <pham.van.toan@sun-asterisk.com>
2020-02-26 11:23:18 +0700

    Top 10 nCOV

--- rasa_backend/webhook/helpers/data_helpers.py
+++ rasa_backend/webhook/helpers/data_helpers.py
@@ -3,7 +3,9 @@ import re
 import lxml.html as lh
 import requests
 from cachetools import cached, TTLCache
+
 from webhook.helpers.date_helpers import to_date
+
 
 @cached(cache=TTLCache(maxsize=1024, ttl=3600))
 def crawler():
@@ -39,7 +41,7 @@ def crawler():
         for t in T.iterchildren():
             data = t.text_content().strip()
 
-            if i in range(1, 6):
+            if i in range(1, 6) and i not in [2, 4]:
                 data = ''.join(c for c in data if c.isdigit())
                 if data:
                     data = int(data)
@@ -52,79 +54,21 @@ def crawler():
     return col, last_updated
 
 
-def summary(col, region_name='All'):
-    try:
-        if region_name == 'All':
-            sum_case = lambda k: sum([int(c) for c in col[k][1]])
-        else:
-            region_index = col[0][1].index(region_name)
-            sum_case = lambda k: int(col[k][1][region_index])
-        total_case = sum_case(1)
-        new_case = sum_case(2)
-        deaths_case = sum_case(3)
-        new_deaths = sum_case(4)
-        recover_case = sum_case(5)
-        return total_case, new_case, deaths_case, new_deaths, recover_case
-    except:
-        return [0] * 5
+def generate_all_message(col, i):
+    name, total, new, death, new_death, recover = [col[j][1][i] for j in range(0, 6)]
+    new = new if new else "+0"
+    new_death = new_death if new_death else "+0"
+    return "Tại {} đã có {} ca nhiễm ({}) tử vong {} ({}) chữa khỏi {}\n".format(name, total, new, death, new_death,
+                                                                                 recover)
 
 
-def get_data(category):
+def get_data():
     col, last_updated = crawler()
-    total_case, new_case, deaths_case, new_deaths, recover_case = summary(col, 'All')
-    vn_total_case, vn_new_case, vn_deaths_case, vn_new_deaths, vn_recover_case = summary(col, 'Vietnam')
-    cn_total_case, cn_new_case, cn_deaths_case, cn_new_deaths, cn_recover_case = summary(col, 'China')
-    sk_total_case, sk_new_case, sk_deaths_case, sk_new_deaths, sk_recover_case = summary(col, 'S. Korea')
-    it_total_case, it_new_case, it_deaths_case, it_new_deaths, it_recover_case = summary(col, 'Italy')
-    ja_total_case, ja_new_case, ja_deaths_case, ja_new_deaths, ja_recover_case = summary(col, 'Japan')
-    # Return the final data.
-    if category == 'deaths':
-        latest, vn_latest, cn_latest, sk_latest, it_latest, ja_latest = deaths_case, vn_deaths_case, cn_deaths_case, sk_deaths_case, it_deaths_case, ja_deaths_case
-    elif category == 'recovered':
-        latest, vn_latest, cn_latest, sk_latest, it_latest, ja_latest = recover_case, vn_recover_case, cn_recover_case, sk_recover_case, it_recover_case, ja_recover_case
-    else:
-        latest, vn_latest, cn_latest, sk_latest, it_latest, ja_latest  = total_case, vn_total_case, cn_total_case, sk_total_case, it_total_case, ja_total_case
-
-    return {
-        'latest': latest,
-        'vn_latest': vn_latest,
-        'cn_latest': cn_latest,
-        'sk_latest': sk_latest,
-        'it_latest': it_latest,
-        'ja_latest': ja_latest,
-        'last_updated': last_updated
-    }
-
-
-def statistic_all():
-    """
-    Return all data statistic and generate message to reply
-    """
-    data = "\n\n".join([statistic(category) for category in ['deaths', 'recovered', 'confirmed']])
-    return data
-
-
-def statistic(category):
-    """
-    Return all data statistic and generate message to reply
-    """
-    category_map = {'deaths': 'tử vong', 'recovered': 'đã được chữa khỏi', 'confirmed': 'bị lây nhiễm'}
-    data = get_data(category)
-    return "Hiện tại đã có {} người {}.\n" \
-           "Tại Việt Nam có {} người\n" \
-           "Tại Trung Quốc có {} người\n" \
-           "Tại Hàn Quốc có {} người\n" \
-           "Tại Italy có {} người\n" \
-           "Tại Nhật Bản có {} người\n" \
-           "Cập nhật mới nhất vào {}".format(
-        data['latest'],
-        category_map[category],
-        data['vn_latest'],
-        data['cn_latest'],
-        data['sk_latest'],
-        data['it_latest'],
-        data['ja_latest'],
-        data['last_updated'])
+    n = 10
+    msg = "Top {} địa danh có tình hình dịch nguy hiểm nhất.\n\n".format(n)
+    for i in range(n):
+        msg += generate_all_message(col, i)
+    msg += "\nCập nhật mới nhất vào {}".format(last_updated)
 
 
 def handle_data(intent):
@@ -135,9 +79,9 @@ def handle_data(intent):
         'ask_all': 'all',
         'fallback': 'fallback'
     }
-    if intent_map[intent] in ["deaths", "recovered", "confirmed"]:
-        return statistic(intent_map[intent])
-    if intent_map[intent] == 'all':
-        return statistic_all()
+    if intent_map[intent] in ["deaths", "recovered", "confirmed", 'all']:
+        return get_data()
     # When fallback
-    return "Chatbot chưa xử lý được nội dung bạn nói. \n\\n Hiện tại chỉ những thông tin sau có thể support. \n1. Thống kê chung tình hình dịch bệnh (VD: Tình hình dịch bệnh hiện nay thế nào) \n2. Hỏi về thống kê số người chết (VD: Có bao nhiêu người chết rồi em)\n3. Hỏi về thống kê số người lây nhiễm (VD: Có bao nhiêu người đã bị lây nhiễm rồi)\n4. Hỏi về thống kê số người được chữa khỏi (VD: Có bao nhiêu người được chữa khỏi rồi)"
+    return "Chatbot chưa xử lý được nội dung bạn nói."
+
+print(handle_data('ask_all'))
